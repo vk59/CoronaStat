f6b274393a33528d4b2329d4b26c440900671af7
ivan <jkostylev592@gmail.com>
2020-08-26 09:48:07 +0300

    Initial commit

---
+++ app/src/main/java/com/vk59/coronastat/ResponseData.java
@@ -0,0 +1,30 @@
+
+package com.vk59.coronastat;
+
+import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+public class ResponseData {
+
+    @SerializedName("countrydata")
+    private List<CountryData> countryData = null;
+    @SerializedName("stat")
+    private String stat;
+
+    public List<CountryData> getCountryData() {
+        return countryData;
+    }
+
+    public void setCountryData(List<CountryData> countryData) {
+        this.countryData = countryData;
+    }
+
+    public String getStat() {
+        return stat;
+    }
+
+    public void setStat(String stat) {
+        this.stat = stat;
+    }
+
+}
