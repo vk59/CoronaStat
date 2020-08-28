604d654368adbcfb8bf3039fc32a3afdbe882101
ivan <jkostylev592@gmail.com>
2020-08-28 08:17:30 +0300

    Returned with findViewById.

--- app/src/main/java/com/vk59/coronastat/presenter/MainPresenter.java
+++ app/src/main/java/com/vk59/coronastat/presenter/MainPresenter.java
@@ -1,19 +1,12 @@
 package com.vk59.coronastat.presenter;
 
-import androidx.databinding.ObservableArrayList;
-import androidx.databinding.ObservableBoolean;
-import androidx.databinding.ObservableList;
-
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 import com.vk59.coronastat.API.APIConfig;
 import com.vk59.coronastat.API.APIService;
 import com.vk59.coronastat.API.APIServiceConstructor;
-import com.vk59.coronastat.model.CountryData;
 import com.vk59.coronastat.model.ResponseData;
 import com.vk59.coronastat.view.IMainView;
-
-import java.util.Observable;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -22,8 +15,6 @@ import retrofit2.Response;
 @InjectViewState
 public class MainPresenter extends MvpPresenter<IMainView> {
     private APIService apiService;
-    public ObservableList<CountryData> countryData = new ObservableArrayList<>();
-    public ObservableBoolean isLoading = new ObservableBoolean();
 
     public MainPresenter() {
         apiService = APIServiceConstructor.CreateService(APIService.class);
@@ -31,22 +22,19 @@ public class MainPresenter extends MvpPresenter<IMainView> {
     }
 
     public void loadData() {
-        isLoading.set(true);
+        getViewState().loading();
         Call<ResponseData> call = apiService.getDataCountry(APIConfig.COUNTRY);
         call.enqueue(new Callback<ResponseData>() {
             @Override
             public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
                 if (response.body() != null) {
-                    countryData.clear();
-                    countryData.addAll(response.body().getCountryData());
+                    getViewState().success(response.body().getCountryData().get(0));
                 }
-                isLoading.set(false);
             }
 
             @Override
             public void onFailure(Call<ResponseData> call, Throwable t) {
                 getViewState().failure();
-                isLoading.set(false);
             }
         });
     }
--- app/src/main/java/com/vk59/coronastat/view/MainActivity.java
+++ app/src/main/java/com/vk59/coronastat/view/MainActivity.java
@@ -1,15 +1,15 @@
 package com.vk59.coronastat.view;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.databinding.DataBindingUtil;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.google.android.material.snackbar.Snackbar;
 import com.vk59.coronastat.R;
-import com.vk59.coronastat.databinding.ActivityMainBinding;
+import com.vk59.coronastat.model.CountryData;
 import com.vk59.coronastat.presenter.MainPresenter;
 
 public class MainActivity extends MvpAppCompatActivity implements IMainView {
@@ -18,24 +18,38 @@ public class MainActivity extends MvpAppCompatActivity implements IMainView {
     public MainPresenter presenter;
 
     private SwipeRefreshLayout layoutRefresh;
-    private ActivityMainBinding binding;
+    private TextView textStatistics;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding =
-                DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.setPresenter(presenter);
+        setContentView(R.layout.activity_main);
+        textStatistics = findViewById(R.id.textStatistics);
+        layoutRefresh = findViewById(R.id.layoutRefresh);
+
+        layoutRefresh.setOnRefreshListener(onUpdateListener);
     }
 
 
     @Override
+    public void loading() {
+        layoutRefresh.setRefreshing(true);
+    }
+
+    @Override
+    public void success(CountryData data) {
+        textStatistics.setText(data.toString());
+        layoutRefresh.setRefreshing(false);
+    }
+
+    @Override
     public void failure() {
-        Snackbar snackbar = Snackbar.make(binding.getRoot(), R.string.message_failure,
+        Snackbar snackbar = Snackbar.make(layoutRefresh, R.string.message_failure,
                 Snackbar.LENGTH_LONG).setAction(R.string.failure_answer,
                 v -> presenter.loadData());
         snackbar.show();
+        layoutRefresh.setRefreshing(false);
     }
 
     private SwipeRefreshLayout.OnRefreshListener onUpdateListener =
@@ -44,5 +58,5 @@ public class MainActivity extends MvpAppCompatActivity implements IMainView {
                 public void onRefresh() {
                     presenter.loadData();
                 }
-            };
+    };
 }
